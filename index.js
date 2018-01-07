9826786e79ee2cfee1f587010f8b63c2cbf24292
dariodlearnco <dario.dorella@gmail.com>
2018-01-07 23:31:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 var recipes = {}
 
+
 // returns an object with the orignal key value pairs and the new key value pair
+// it does not modify the original object, but rather returns a clone with
+// the new dat
 function updateObjectWithKeyAndValue(object, key, value) {
     return Object.assign({}, object, {[key]: value})
 }
+
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+    // updates `object` with the given `key` and `value` (it is destructive)
+    // and returns the entire updated object
+    return Object.assign(object, {[key]: value})
