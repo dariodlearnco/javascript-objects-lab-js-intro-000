e2e4103a519f785b2a567fe99dd6fd5ffa5f7d4f
dariodlearnco <dario.dorella@gmail.com>
2018-01-07 23:30:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ var recipes = {}
 
 // returns an object with the orignal key value pairs and the new key value pair
 function updateObjectWithKeyAndValue(object, key, value) {
-    return Object.assign(object, {[key]: value})
+    return Object.assign({}, object, {[key]: value})
 }
